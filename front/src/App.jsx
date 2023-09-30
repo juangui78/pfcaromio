b04b48de0fef889ef7472ac99b81ccf40a72addc
Juan Parra <juangui902@gmail.com>
2023-09-29 21:18:30 -0500

    getting home component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css'
 import LoginForm from './components/Login/Login'
 import Logout from './components/Logout/Logout'
-import Profile from './components/Profile/Profile'import LandingPage from '../src/components/LandingPage/LandingPage';
+import Profile from './components/Profile/Profile'
+import LandingPage from '../src/components/LandingPage/LandingPage';
 import Home from '../src/components/Home/Home'
 
 const App = () => {
