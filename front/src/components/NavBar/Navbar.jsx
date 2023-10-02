db769ba62a60374211052bab2691abe1898fc2d9
julian-nac <julian.narvaezc@gmail.com>
2023-10-02 15:23:13 -0500

    Boton Volver A Inicio En Products

--- front/src/components/NavBar/Navbar.jsx
+++ front/src/components/NavBar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   const [filtersDropdownOpen, setFiltersDropdownOpen] = useState(false);
+  const location = useLocation();
+
+  const showFiltersAndSearch = !location.pathname.startsWith('/products');
 
   const toggleFiltersDropdown = () => {
     setFiltersDropdownOpen(!filtersDropdownOpen);
@@ -15,51 +18,65 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <div className="logo">
-          <img src="LogoPizzeria.png" alt="Logo" />
+          <Link to='/home'>
+                    <img src="LogoPizzeria.png" alt="Logo" />
+          </Link>
         </div>
 
-        <div className="filters">
-          <div className="dropdown">
-            <button className="filter-button filter-buttonns" onClick={toggleFiltersDropdown}>
-              Agregar filtros
-            </button>
-            {filtersDropdownOpen && (
-              <div className="dropdown-content">
-                <div className="filter-button">
-                  <button>Filtrar por Rating</button>
-                  <div className="dropdown-content-inner show-scroll">
-                    <a href="#">Peor Rating</a>
-                    <a href="#">Mejor Rating</a>
-                  </div>
-                </div>
-
-                <div className="filter-button">
-                  <button>Filtrar por Precio</button>
-                  <div className="dropdown-content-inner show-scroll">
-                    <a href="#">Menor Precio</a>
-                    <a href="#">Mayor Precio</a>
-  
-                  </div>
-                </div>
-
-                <div className="filter-button">
-                  <button>Filtrar por Nombre</button>
-                  <div className="dropdown-content-inner show-scroll">
-                    <a href="#">Z-A</a>
-                   <a href="#">A-Z</a>
-                  </div>
-                </div>
-              </div>
-            )}
+        {!showFiltersAndSearch && (
+          <div className="back-to-home-button" style={{ marginRight: '545px', borderBottom: '1px solid black', position: 'relative' }}>
+          <Link to="/home" style={{ textDecoration: 'none', color: 'black', fontSize: '16px' }}>
+              Volver a Inicio
+            </Link>
           </div>
-        </div>
+        )}
 
-        <div className="search-bar">
-          <input type="text" placeholder="Buscar comidas, restaurantes..." />
-          <button>
-            <img src="Lupa.png" alt="Buscar" className="lupa" />
-          </button>
-        </div>
+        {showFiltersAndSearch && (
+          <>
+            <div className="filters">
+              <div className="dropdown">
+                <button className="filter-button filter-buttonns" onClick={toggleFiltersDropdown}>
+                  Agregar filtros
+                </button>
+                {filtersDropdownOpen && (
+                  <div className="dropdown-content">
+                    <div className="filter-button">
+                      <button>Filtrar por Rating</button>
+                      <div className="dropdown-content-inner show-scroll">
+                        <a href="#">Peor Rating</a>
+                        <a href="#">Mejor Rating</a>
+                      </div>
+                    </div>
+
+                    <div className="filter-button">
+                      <button>Filtrar por Precio</button>
+                      <div className="dropdown-content-inner show-scroll">
+                        <a href="#">Menor Precio</a>
+                        <a href="#">Mayor Precio</a>
+                      </div>
+                    </div>
+
+                    <div className="filter-button">
+                      <button>Filtrar por Nombre</button>
+                      <div className="dropdown-content-inner show-scroll">
+                        <a href="#">Z-A</a>
+                        <a href="#">A-Z</a>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="search-bar">
+              <input type="text" placeholder="Buscar comidas, restaurantes..." />
+              <button>
+                <img src="Lupa.png" alt="Buscar" className="lupa" />
+              </button>
+            </div>
+          </>
+        )}
+
         <div className="user-actions">
           {!isAuthenticated ? (
             <button className="login-button" onClick={() => loginWithRedirect()}>Iniciar Sesión</button>
